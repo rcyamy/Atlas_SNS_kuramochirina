6fca133b2a79d741874d6cac1684b61941c325fd
倉持莉奈 <>
2022-11-15 10:59:21 +0900

    アコーデオンメニューの記述が途中まであっているか確認をお願いいたします。

--- AtlasSNS/resources/views/layouts/login.blade.php
+++ AtlasSNS/resources/views/layouts/login.blade.php
@@ -22,17 +22,20 @@
 <body>
     <header>
         <div id = "head">
-        <h1><a><img src="images/logo.png"></a></h1>
+        <h1><a href="/top"><img src="images/logo.png"></a></h1>
             <div id="">
                 <div id="">
                     <p>〇〇さん<img src="images/arrow.png"></p>
                 <div>
-                <ul>
-                    <li><a href="/top">ホーム</a></li>
-                    <li><a href="/profile">プロフィール</a></li>
-                    <li><a href="/logout">ログアウト</a></li>
-                </ul>
             </div>
+            <div id="accordion" class="accordion-container">
+  <h4 class="accordion-title js-accordion-title"></h4>
+  <div class="accordion-content">
+    <li><a href="/top">HOME</a></li>
+    <li><a href="/profile">プロフィール編集</a></li>
+    <li><a href="/logout">ログアウト</a></li>
+          </div><!--/.accordion-content-->
+         </div><!--/#accordion-->
         </div>
     </header>
     <div id="row">
@@ -46,19 +49,20 @@
                 <p>フォロー数</p>
                 <p>〇〇名</p>
                 </div>
-                <p class="btn"><a href="">フォローリスト</a></p>
+                <p class="btn"><a href="/follow-list">フォローリスト</a></p>
                 <div>
                 <p>フォロワー数</p>
                 <p>〇〇名</p>
                 </div>
-                <p class="btn"><a href="">フォロワーリスト</a></p>
+                <p class="btn"><a href="/follower-list">フォロワーリスト</a></p>
             </div>
-            <p class="btn"><a href="">ユーザー検索</a></p>
+            <p class="btn"><a href="/search">ユーザー検索</a></p>
         </div>
     </div>
     <footer>
     </footer>
-    <script src="JavaScriptファイルのURL"></script>
-    <script src="JavaScriptファイルのURL"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+
 </body>
 </html>
